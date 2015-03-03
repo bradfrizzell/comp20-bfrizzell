6bc0d533cbb124e001726690dc70a69ab4e34779
Brad <bradleyfrizzell@gmail.com>
2015-03-03 14:56:07 -0500

    Implemented js to draw board and Ms. Pacman

--- Lab5/mspacman/game.js
+++ Lab5/mspacman/game.js
@@ -1,1 +1,13 @@
-// Replace this with your code...
+function init() {
+    var canvas = document.getElementById("game_canvas");
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        var img = new Image();
+        img.onload = function(){
+        ctx.drawImage(img,320,0, 550, 140, 0,0, 550, 140);
+        ctx.drawImage(img,75,17, 25, 25, 30,25, 25, 25);
+        }
+    };
+  img.src = 'pacman10-hp-sprite.png';
+  }
+     

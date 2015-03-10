59ea66ee599fa5e1868fe63fad40438a91d83e69
Brad <brad@brad-Inspiron-545.(none)>
2015-03-10 18:11:41 -0400

    deleted init function

--- messages/lab.js
+++ messages/lab.js
@@ -1,26 +1,19 @@
-// Your JavaScript goes here...
-
-function init() {
-
-
-  }
-
 function parse(){
-request = new XMLHttpRequest();
-		request.open("get", "data.json", true);
-		request.onreadystatechange = myCallbackFunction;
-		request.send();
-		function myCallbackFunction() {
-			if (request.readyState == 4 && request.status == 200) {
-				data = JSON.parse(request.responseText);
-				for(i=0;i<2;i++) {
-				phrase= data[i].content +" "+ data[i].username;
+    request = new XMLHttpRequest();
+    request.open("get", "data.json", true);
+    request.onreadystatechange = myCallbackFunction;
+    request.send();
+    function myCallbackFunction() {
+	if (request.readyState == 4 && request.status == 200) {
+	    data = JSON.parse(request.responseText);
+	    for(i=0;i<2;i++) {
+		phrase= data[i].content +" "+ data[i].username;
 					
-	document.getElementById("messages").innerHTML = document.getElementById("messages").innerHTML+ '</br>' + phrase;
+		document.getElementById("messages").innerHTML = document.getElementById("messages").innerHTML+ '</br>' + phrase;
 					
-				}
-			}
-		}
+	    }
+	}
+    }
 }
 
 

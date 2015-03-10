b5bfbd0f652bd1fef43d6e2502474e458c8fb741
Brad <brad@brad-Inspiron-545.(none)>
2015-03-10 18:19:53 -0400

    Fixed number of messages pritned

--- messages/lab.js
+++ messages/lab.js
@@ -1,17 +1,15 @@
 function parse(){
     myRequest = new XMLHttpRequest();
     myRequest.open("get", "data.json", true);
-    myRequest.onreadystatechange = myCallbackFunction;
+    myRequest.onreadystatechange = writeMessageCallback;
     myRequest.send();
-    function myCallbackFunction() {
+    function writeMessageCallback() {
 	messages = document.getElementById("messages");
 	if (myRequest.readyState == 4 && myRequest.status == 200) {
 	    data = JSON.parse(myRequest.responseText);
-	    for(i=0;i<2;i++) {
-		phrase= data[i].content +" "+ data[i].username;
-					
+	    for(i=0;i<data.length;i++) {
+		phrase= data[i].content +" "+ data[i].username;				
 		messages.innerHTML = messages.innerHTML+ '</br>' + phrase;
-					
 	    }
 	}
     }

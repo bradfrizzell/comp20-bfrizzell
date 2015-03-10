a3f2f6f1324ae2761ebee5fa463fbee22b981f7e
Brad <brad@brad-Inspiron-545.(none)>
2015-03-10 18:23:14 -0400

    Modifying modularity of message printing

--- messages/lab.js
+++ messages/lab.js
@@ -7,6 +7,7 @@ function parse(){
 	messages = document.getElementById("messages");
 	if (myRequest.readyState == 4 && myRequest.status == 200) {
 	    data = JSON.parse(myRequest.responseText);
+	    console.log(data.length);
 	    for(i=0;i<data.length;i++) {
 		phrase= data[i].content +" "+ data[i].username;				
 		messages.innerHTML = messages.innerHTML+ '</br>' + phrase;

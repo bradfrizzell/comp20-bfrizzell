b1c33cd2e52d4e37b4d19bcb90571ff70a727129
Brad <brad@brad-Inspiron-545.(none)>
2015-03-10 18:15:35 -0400

    Testing a clean up of syntax

--- messages/lab.js
+++ messages/lab.js
@@ -1,15 +1,16 @@
 function parse(){
-    request = new XMLHttpRequest();
-    request.open("get", "data.json", true);
-    request.onreadystatechange = myCallbackFunction;
-    request.send();
+    myRequest = new XMLHttpRequest();
+    myRequest.open("get", "data.json", true);
+    myRequest.onreadystatechange = myCallbackFunction;
+    myRequest.send();
     function myCallbackFunction() {
-	if (request.readyState == 4 && request.status == 200) {
-	    data = JSON.parse(request.responseText);
+	messages = document.getElementById("messages");
+	if (myRequest.readyState == 4 && myRequest.status == 200) {
+	    data = JSON.parse(myRequest.responseText);
 	    for(i=0;i<2;i++) {
 		phrase= data[i].content +" "+ data[i].username;
 					
-		document.getElementById("messages").innerHTML = document.getElementById("messages").innerHTML+ '</br>' + phrase;
+		messages.innerHTML = messages.innerHTML+ '</br>' + phrase;
 					
 	    }
 	}
